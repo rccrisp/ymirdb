ad13bc59d6d1a6515f99ee65fb37af3f5f3d5596
Rex <rcri3020@uni.sydney.edu.au>
2022-03-29 10:10:39 +1100

    general entries

--- ymirdb.c
+++ ymirdb.c
@@ -210,9 +210,9 @@ bool populate_values(entry ** ptr, entry * this_entry, char * new_values[], int 
 	// if we have gone through all the values, and all are valid, add them to this entry
 	for(int i = index; i < size; i++){
 		this_entry->values[i] = entry_values[i];
-		this_entry->forward = malloc(sizeof(entry**));
+		this_entry->forward = malloc(sizeof(entry*));
 		this_entry->forward_size = 0;
-		this_entry->backward = malloc(sizeof(entry**));
+		this_entry->backward = malloc(sizeof(entry*));
 		this_entry->backward_size = 0;
 		if(entry_values[i].type == 1){
 			include_entry_in_values(this_entry,entry_values[i].entry);
