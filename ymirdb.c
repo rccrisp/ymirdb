ece2b4e4a672b97815fd5d096df3e731d013494f
Rex <rcri3020@uni.sydney.edu.au>
2022-03-31 17:59:34 +1100

    len

--- ymirdb.c
+++ ymirdb.c
@@ -1143,18 +1143,13 @@ void command_checkout(char * line, entry ** ptr, snapshot ** snapshots){
 			// copy the forward references
 			this_entry->forward_size = iter->forward_size;
 			this_entry->forward = malloc(sizeof(entry*)*this_entry->forward_size);
-			memcpy(this_entry->forward,iter->forward,sizeof(entry*)*this_entry->forward_size);
-			// for(int i = 0; i < this_entry->forward_size; i++){
-			// 	this_entry->forward[i] = iter->forward[i];
-			// }
+			memmove(this_entry->forward,iter->forward,sizeof(entry*)*this_entry->forward_size);
+
 
 			// copy the backward references
 			this_entry->backward_size = iter->backward_size;
 			this_entry->backward = malloc(sizeof(entry*)*this_entry->backward_size);
-			memcpy(this_entry->backward,iter->backward,sizeof(entry*)*this_entry->backward_size);
-			// for(int i = 0; i < this_entry->backward_size; i++){
-			// 	this_entry->backward[i] = iter->backward[i];
-			// }
+			memmove(this_entry->backward,iter->backward,sizeof(entry*)*this_entry->backward_size);
 
 			// copy the length
 			this_entry->length = iter->length;
